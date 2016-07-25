a02849fcd71d7d90a747bf65969acbaaf15a68be
dragon <rongjihuang@gmail.com>
2016-07-25 17:23:46 +0800

    init

---
+++ src/main/java/cn/bc/rest/RootResource.java
@@ -0,0 +1,18 @@
+package cn.bc.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * rest 服务的根访问路径对应的资源
+ *
+ * @author dragon 2016-07-14
+ */
+public class RootResource {
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String root() {
+		return "bc rest service";
+	}
+}
